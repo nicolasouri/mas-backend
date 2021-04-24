314d4f34d054686b1a900605483c2c4274248f9a
nicolasouri <50713392+nicolasouri@users.noreply.github.com>
2021-04-24 14:38:06 -0300

    commit semanal
    adding routes, orm, server, httpreq

---
+++ src/routes.ts
@@ -0,0 +1,38 @@
+import {Router} from 'express'
+
+const routes = Router();
+
+routes.get('/user', (request, response) => response.json({
+    message: 'Hello World'
+}))
+
+routes.get('/user/:id/', (request, response) => {
+    const {id} = request.params;
+
+    response.json({
+        userId:id
+    })
+})
+
+routes.get('/user/', (request, response) => {
+    const {nome, idade} = request.query
+    
+    response.json({
+        nome,
+        idade
+    })
+})
+
+routes.post('/user', (request, response) => {
+    const {name, email, password} = request.body
+
+    const user = {
+        name,
+        email,
+        password
+    } 
+    return response.json(user);
+})
+    
+
+export default routes;
